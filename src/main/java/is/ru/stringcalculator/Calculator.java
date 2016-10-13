10ce78072dc8d5a9a8d35e5940c1d306ab2a2d24
andrikarel <karelandri@gmail.com>
2016-10-13 15:38:59 +0000

    added test for one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,33 +1,15 @@
 package is.ru.stringcalculator;
 
-public class Calculator {
+public class Calculator 
+{
 
-	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		}
-		else
+	public static int add(String text)
+	{
+		
 			return 1;
 	}
 
-	private static int toInt(String number){
-		return Integer.parseInt(number);
-	}
-
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
-	}
-      
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
-		}
-		return total;
-    }
+	
 
 
 
